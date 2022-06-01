e8fcd4ac9ea12d9c0d96a49302eb31faa15e2129
Greg1928 <gregoriosortino@protonmail.com>
2022-06-01 17:54:18 +0200

    added main

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+      <div class="container">
+          <p>-->content goes here--</p>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: "BaseMain",
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+    background-color: var(--secondary-color);
+
+    & .container p{
+        padding: 4.375rem 0;
+        color: white;
+        font-size: 2.5rem;
+    }
+}
+</style>
