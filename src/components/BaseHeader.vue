32211f5baae3f03d6916ea54e59739b6f8e162a0
Greg1928 <gregoriosortino@protonmail.com>
2022-06-02 20:10:18 +0200

    added dynamic nav bar

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -7,16 +7,7 @@
         </div>
         <div class="header__nav">
             <ul>
-                <li><a href="#">link</a></li>
-                <li class="active"><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
-                <li><a href="#">link</a></li>
+                <li v-for="(link, i) in links" :key="i" :class="{active : link.current}"><a :href="link.url" @click="current(i)">{{link.text}}</a><div class="bar" :class="{block : link.current}"></div></li>
             </ul>
         </div>
        </div>
@@ -26,7 +17,80 @@
  <script>
  export default {
     name: "baseHeader",
- }
+    data() {
+        return {
+            index: 0,
+            links: [
+                {
+                    text: "characters",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "comics",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "movies",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "tv",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "games",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "collectibles",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "videos",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "fans",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "news",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "shops",
+                    url: "#",
+                    current: false,
+                },
+                ]
+        }
+    },
+    methods:
+    {
+        current(i){
+            this.index = i;
+            for(let i = 0; i<this.links.length; i++){
+                if(this.links[i].current === true){
+                    this.links[i].current = false;
+            }
+            }
+            this.links[i].current = true;
+                
+            
+            }
+            
+    },
+    }
+
  </script>
  
  <style lang="scss" scoped>
@@ -40,22 +104,45 @@
 .container{
     @include InLine;
     justify-content: space-between;
-    padding: 0.7rem 0;
-
+    height: 8.125rem;
     & .header__nav{
         align-self: center;
+        margin-top: calc(8.125rem/2);
     } 
     & ul{
         @include InLine;
-        column-gap: 1rem;
+        column-gap: 1.3rem;
     }
     a{
-        padding: 1rem;
         text-decoration: none;
         color: black;
+        text-transform: uppercase;
+        font-size: .8125rem;
+        display: flex;
     }
     .active a{
         color: var(--primary-color);
+    }
+    .bar{
+        height: 0.3rem;
+        background-color: var(--primary-color);
+        display: none;
+    }
+    li{
+        display: flex;
+        flex-direction: column;
+        height: calc(8.125rem/2);
+        justify-content: space-between;
+    }
+    .header__logo{
+        display: flex;
+        align-items: center;
+    }
+    img{
+        height: 5.500rem;
+    }
+    .block{
+        display: block;
     }
 }
 
--- src/components/footer/BaseFooterTop.vue
+++ src/components/footer/BaseFooterTop.vue
@@ -3,11 +3,11 @@
       <div class="footer-top">
           <div class="container">
               <ul>
-                  <li><img class="Png" src="../../assets/img/buy-comics-digital-comics.png" alt=""><a href="#">link</a></li>
-                  <li><img class="Png" src="../../assets/img/buy-comics-merchandise.png" alt=""><a href="#">link</a></li>
-                  <li><img class="Png" src="../../assets/img/buy-comics-subscriptions.png" alt=""><a href="#">link</a></li>
-                  <li><img class="Png" src="../../assets/img/buy-comics-shop-locator.png" alt=""><a href="#">link</a></li>
-                  <li><img src="../../assets/img/buy-dc-power-visa.svg" alt=""><a href="#">link</a></li>
+                  <li><img class="Png" src="../../assets/img/buy-comics-digital-comics.png" alt=""><a href="#">digital comics</a></li>
+                  <li><img class="Png" src="../../assets/img/buy-comics-merchandise.png" alt=""><a href="#">dc merchandise</a></li>
+                  <li><img class="Png" src="../../assets/img/buy-comics-subscriptions.png" alt=""><a href="#">subscription</a></li>
+                  <li><img class="Png" src="../../assets/img/buy-comics-shop-locator.png" alt=""><a href="#">comic shop locator</a></li>
+                  <li><img src="../../assets/img/buy-dc-power-visa.svg" alt=""><a href="#">dc power visa</a></li>
               </ul>
           </div>
       </div>
@@ -32,17 +32,19 @@ footer{
     }
     .container ul{
         @include InLine;
-        justify-content: space-around;
+        justify-content: space-between;
+        padding: 0 0.5rem;
     }
     li{
         display: flex;
         align-items: center;
-        column-gap: 1rem;
+        column-gap: .6rem;
     }
     a{
         text-decoration: none;
         color: white;
-        font-size: 1.25rem;
+        font-size: .855rem;
+        text-transform: uppercase;
     }
     img{
         height: 2.5rem;
